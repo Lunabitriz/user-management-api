939ddbf88fb55fbeb3239e43d9848e769519d0e6
Luna Beatriz <145813826+Lunabitriz@users.noreply.github.com>
2025-10-14 10:44:50 -0300

    Implementação do Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/**/*.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
